544b541f3f9135616afa6395a2d21d6e89bda862
Lucas Huber <lucas.huber.1999@gmail.com>
2022-12-08 12:35:42 +0100

    CHANGED 2022/day08; ADDED documentation

--- 2022/day08/day08.py
+++ 2022/day08/day08.py
@@ -2,14 +2,24 @@ from os import path
 file = open(path.join(path.dirname(__file__), "input.txt"))
 
 lines = file.read().split("\n")
-x_max, y_max = len(lines[0]) - 1, len(lines) - 1	# index maxima
-visible = 2 * len(lines[0]) + 2 * len(lines) - 4	# outer trees
 
-def check_single_direction_visible(x: int, y: int, from_max: bool, x_direction: bool) -> bool:
+def check_single_direction_visible(x: int, y: int, max: int, from_max: bool, x_direction: bool) -> bool:
+    """Check if tree will be visible in a given direction (every height in row/column smaller than own height)
+
+    Args:
+        x (int): x of tree
+        y (int): y of tree
+        max (int): max of index in given direction
+        from_max (bool): Check visibility from max or min index to tree
+        x_direction (bool): Check in x or y direction
+
+    Returns:
+        bool: If tree is visible in specified direction
+    """
     height = int(lines[y][x])
     if x_direction:
         initial = x
-        x = x_max if from_max else 0
+        x = max if from_max else 0
         while x > initial if from_max else x < initial:
             if int(lines[y][x]) >= height:
                 break
@@ -19,7 +29,7 @@ def check_single_direction_visible(x: int, y: int, from_max: bool, x_direction: 
         return False
     else:
         initial = y
-        y = y_max if from_max else 0
+        y = max if from_max else 0
         while y > initial if from_max else y < initial:
             if int(lines[y][x]) >= height:
                 break
@@ -28,55 +38,85 @@ def check_single_direction_visible(x: int, y: int, from_max: bool, x_direction: 
                 return True
         return False
 
+x_max, y_max = len(lines[0]) - 1, len(lines) - 1	# index maxima
 def check_if_visible(x: int, y: int) -> bool:
-    left = check_single_direction_visible(x, y, from_max=False, x_direction=True)
-    right = check_single_direction_visible(x, y, from_max=True, x_direction=True)
-    up = check_single_direction_visible(x, y, from_max=False, x_direction=False)
-    down = check_single_direction_visible(x, y, from_max=True, x_direction=False)
+    """Check if tree is visible from any direction
+
+    Args:
+        x (int): x of tree
+        y (int): y of tree
+
+    Returns:
+        bool: If tree is visible from any direction
+    """
+    left = check_single_direction_visible(x, y, x_max, from_max=False, x_direction=True)
+    right = check_single_direction_visible(x, y, x_max, from_max=True, x_direction=True)
+    up = check_single_direction_visible(x, y, y_max, from_max=False, x_direction=False)
+    down = check_single_direction_visible(x, y, y_max, from_max=True, x_direction=False)
     if left or right or up or down:
         return True
     else:
         return False
 
-
-for yt in range(1, len(lines) - 1):
-    for xt in range(1, len(lines[0]) - 1):
-        visible += 1 if check_if_visible(xt, yt) else 0
+visible = 2 * len(lines[0]) + 2 * len(lines) - 4	# outer trees
+for y in range(1, len(lines) - 1):
+    for x in range(1, len(lines[0]) - 1):
+        visible += 1 if check_if_visible(x, y) else 0
         
 print("Part One: {}".format(visible))
 
 # ----------------------------------------------------------------------------------------------------
 
-def get_single_direction_score(x: int, y: int, to_max: bool, x_direction: bool) -> int:
+def get_single_direction_score(x: int, y: int, max_index: int, to_max: bool, x_direction: bool) -> int:
+    """Check scenic score of tree in a given direction
+
+    Args:
+        x (int): x of tree
+        y (int): y of tree
+        max_index (int): max of index in the given direction
+        to_max (bool): Check scenic score in direction of max of min index
+        x_direction (bool): Check in x or y direction
+
+    Returns:
+        int: Scenic score for the specified direction
+    """
     height = int(lines[y][x])
     s = 0
     if x_direction:
-        while x < x_max if to_max else x > 0:
+        while x < max_index if to_max else x > 0:
             x += 1 if to_max else -1
             s += 1
             if int(lines[y][x]) >= height:
                 break
         return s
     else:
-        while y < y_max if to_max else y > 0:
+        while y < max_index if to_max else y > 0:
             y += 1 if to_max else -1
             s += 1
             if int(lines[y][x]) >= height:
                 break
         return s
     
+def get_scenic_score(x: int, y: int) -> int:
+    """Gets the total scenic score for a tree
 
-def get_scenic_score(x: int, y: int):
-    left = get_single_direction_score(x, y, to_max=False, x_direction=True)
-    right = get_single_direction_score(x, y, to_max=True, x_direction=True)
-    up = get_single_direction_score(x, y, to_max=False, x_direction=False)
-    down = get_single_direction_score(x, y, to_max=True, x_direction=False)
+    Args:
+        x (int): x of tree
+        y (int): y of tree
+
+    Returns:
+        int: Total scenic score of given tree
+    """
+    left = get_single_direction_score(x, y, x_max, to_max=False, x_direction=True)
+    right = get_single_direction_score(x, y, x_max, to_max=True, x_direction=True)
+    up = get_single_direction_score(x, y, y_max, to_max=False, x_direction=False)
+    down = get_single_direction_score(x, y, y_max, to_max=True, x_direction=False)
     
     return left * right * up * down
 
 score = 0
-for yt in range(1, len(lines) - 1):
-    for xt in range(1, len(lines[0]) - 1):
-        score = max(score, get_scenic_score(xt, yt))
+for y in range(1, len(lines) - 1):
+    for x in range(1, len(lines[0]) - 1):
+        score = max(score, get_scenic_score(x, y))
             
 print("Part Two: {}".format(score))
